d8097423f1322e5352cfd82dced8aa7aa694a506
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-06-28 10:24:39 -0400

    all the work

---
+++ src/js/modules/scroll.js
@@ -0,0 +1,61 @@
+var windowTop, windowHeight, stepToShow;
+
+module.exports =  {
+    init: function() {
+        this.bindings();
+    },
+
+    bindings: function() {
+        $(window).scroll(function() {
+            this.onScroll();
+        }.bind(this))
+    },
+
+    onScroll: function() {
+        this.updateValues();
+        this.fixMap();
+        this.setStep();
+    },
+
+    updateValues: function() {
+        windowTop = window.pageYOffset || document.documentElement.scrollTop;
+        windowHeight = $(window).height();
+        mapHeight = $('.uit-map').height();
+    },
+
+    fixMap: function() {
+        console.log('fixing');
+        if (windowTop > $('.uit-map__point').offset().top - this.percentageOfHeight(20)) {
+            $('.uit-map').addClass('is-fixed');
+            $('.uit-map__point').attr('style', 'margin-bottom:' + mapHeight + 'px;');
+        } else {
+            $('.uit-map').removeClass('is-fixed');
+            $('.uit-map__point').removeAttr('style');
+        }
+    },
+
+    setStep: function() {
+        stepToShow = null;
+
+        $('.uit-step').each(function(i, el) {
+            if (windowTop > $(el).offset().top - this.percentageOfHeight(20)) {
+                stepToShow = $(el).data('step');
+            }
+        }.bind(this));
+
+        // remove current step
+        $('.uit-map').removeClass(function(i, className) {
+            if (className) {
+                return (className.match(/uit-map-state--[0-9]{1,2}/g) || []).join(' ');
+            }
+        });
+
+        if (stepToShow !== null) {
+            $('.uit-map').addClass('uit-map-state--' + stepToShow);
+        }
+    },
+
+    percentageOfHeight: function(percentage) {
+        return (windowHeight / 100) * percentage;
+    }
+};
